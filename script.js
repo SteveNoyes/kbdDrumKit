3cd078bd6a57d8f0ddab2d6512f76e0cbe569de5
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-08-21 18:36:34 -0400

    stock photos

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function playSound(anything) { // function to play sound takes one variable
   if(!audio){
    return;
   };
+
   audio.currentTime = 0; // to start of audio file on keydown
   audio.play(); // play audio
   key.classList.add('playing'); // add the class list of playing to the key pressed
 }
+
 // function to remove class playing from key at the end of transition
 function removeTransition(e) {
   // looking through the key:value pairs of 'this' key
@@ -17,9 +19,11 @@ function removeTransition(e) {
   if (e.propertyName !== 'transform') {
     return;
   };
+
   // else remove playing class from 'this' key
   this.classList.remove('playing');
 }
+
 // create variable leys to hold everything with the class key
 const keys = document.querySelectorAll('.key');
 // iterate through the div keys and for each of them at an event listener
